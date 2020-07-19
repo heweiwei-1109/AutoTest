785018bedf560e5cd56e9f30361a15da7dcb5e54
hewei001 <hewei@changyin.sh.cn>
2020-07-19 19:05:34 +0800

    变量+非构造函数+构造函数

--- java_auto/src/com/he/day03/IfDemo.java
+++ java_auto/src/com/he/day03/IfDemo.java
@@ -91,7 +91,7 @@ public class IfDemo {
             }
         }
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            System.out.print("  "+arr[i]);
         }
     }
 }
